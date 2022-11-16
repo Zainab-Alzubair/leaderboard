7039542cdc8a7c1306468696b2e4f4a335f948a6
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-11-16 14:19:11 +0300

    Add page structure

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  
   optimization: {
     runtimeChunk: 'single',
   },
