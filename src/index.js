03e98b8b473f0469500834f85a519efb4a5fb5c8
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2022-11-18 03:36:52 +0300

    Added api functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-// import _ from 'lodash';
 import './style.css';
+import fetchData from './modules/getData.js';
+import postData from './modules/postData.js';
+
+const refresh = document.querySelector('.refresh-btn');
+const form = document.getElementById('form');
+const userInput = document.getElementById('user');
+const scoreInput = document.getElementById('score');
+
+// submit form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postData();
+  userInput.value = '';
+  scoreInput.value = '';
+});
+
+// refresh DOM
+refresh.addEventListener('click', () => {
+  fetchData()
+    .then((data) => {
+      let result = '';
+      data.result.forEach((elem) => {
+        result += `<div class="table-row">${elem.user} | ${elem.score}</div>`;
+      });
+      document.querySelector('.table').innerHTML = result;
+    });
+});
